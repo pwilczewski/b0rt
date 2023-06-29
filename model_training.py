7630ba30aed58e093a234754762e5542b0df1fd3
pwilczewski <pwilczewski@users.noreply.github.com>
2023-06-29 14:11:57 -0400

    separate code for model training and evaluation

--- model_training.py
+++ model_training.py
@@ -1,16 +1,12 @@
 import torch
-from transformers import BertTokenizer, BertForMaskedLM, BertConfig
-from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from transformers import BertTokenizer, BertForMaskedLM
 from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
-from transformers import pipeline
 
 # Load the tokenizer and model
-tokenizer = BertTokenizer.from_pretrained('b0rt')
-model = BertForMaskedLM.from_pretrained('b0rt')
-
-# add a layer 768x768 to train, skip connection!
-# try LoRA
+tokenizer = BertTokenizer.from_pretrained('bert_orig')
+model = BertForMaskedLM.from_pretrained('bert_orig')
 
 # freeze every layer except the embedding layer
 for param in model.parameters():
@@ -19,31 +15,16 @@ for param in model.parameters():
     else:
         param.requires_grad = False
 
-train_dataset = LineByLineTextDataset(
-    tokenizer=tokenizer,file_path='training_data.txt', block_size=128)
+train_dataset = LineByLineTextDataset(tokenizer=tokenizer,file_path='training_data.txt', block_size=128)
 
+# set up collator for MLM
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
 
-import torch
-import torch.nn.functional as F
-
-# is my code even using dataloader?
-# train_dataloader = DataLoader(
-    #train_dataset, batch_size=16, shuffle=True, collate_fn=custom_collate_fn)
-
+# train for 3 epochs
 training_args = TrainingArguments(output_dir='./bert_finetuned_logs', 
-                                  num_train_epochs=3, 
-                                  per_device_train_batch_size=64)
+                                  num_train_epochs=3, per_device_train_batch_size=64)
 
-# Initialize the Trainer
-trainer = Trainer(model=model,args=training_args,
-    data_collator=data_collator,train_dataset=train_dataset,)
+trainer = Trainer(model=model,args=training_args,data_collator=data_collator,train_dataset=train_dataset,)
 
-# Start training
-trainer.train()
-
-# Save the trained model
-trainer.save_model('./bert_finetuned1')
-
-# can I use a pytorch trainer?
-plz = pipeline('fill-mask', model='bert_finetuned1')
+# trainer.train()
+# trainer.save_model('./bert_finetuned')
